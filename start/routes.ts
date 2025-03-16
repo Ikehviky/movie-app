a7de21087bc0fb196bf6830557c0513b13874c42
somtooffor <somto.offor@opexconsult.co.uk>
2025-03-17 00:28:53 +0100

    going through the foundations of adonis

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,26 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
+import { Exception } from '@adonisjs/core/exceptions'
+import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
+import fs from 'node:fs/promises'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
+
+router
+  .get('/movie/:slug', async (ctx) => {
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    try {
+      const movie = await fs.readFile(url, 'utf-8')
+      ctx.view.share({ movie })
+    } catch (error) {
+      throw new Exception(`could not find movie called ${ctx.params.slug}`, {
+        code: 'E_NOT_FOUND',
+        status: 404,
+      })
+    }
+    return ctx.view.render('pages/movies/show')
+  })
+  .as('movies.show')
+  .where('slug', router.matchers.slug())
