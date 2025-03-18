e17ef3885c2910d0a10b3c8515ef376afc9394e6
somtooffor <somto.offor@opexconsult.co.uk>
2025-03-18 03:38:40 +0100

    added services

---
+++ app/services/movie_service.ts
@@ -0,0 +1,39 @@
+import { Exception } from '@adonisjs/core/exceptions'
+import app from '@adonisjs/core/services/app'
+import { MarkdownFile } from '@dimerapp/markdown'
+import { readdir, readFile } from 'node:fs/promises'
+
+export class MovieService {
+  // Your code here
+
+  static getSlugUrl(slug: string) {
+    if (!slug.endsWith('.md')) {
+      slug += '.md'
+    }
+
+    return app.makeURL(`resources/movies/${slug}`)
+  }
+
+  static async getSlug() {
+    const files = await readdir(app.makeURL('resources/movies'))
+    return files.map((file) => file.replace('.md', ''))
+  }
+
+  static async read(slug: string) {
+    try {
+      const url = this.getSlugUrl(slug)
+      const file = await readFile(url, 'utf-8')
+
+      const md = new MarkdownFile(file)
+
+      await md.process()
+
+      return md
+    } catch (error) {
+      throw new Exception(`could not find movie called ${slug}`, {
+        code: 'E_NOT_FOUND',
+        status: 404,
+      })
+    }
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -6,31 +6,25 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-import { Exception } from '@adonisjs/core/exceptions'
-import app from '@adonisjs/core/services/app'
+
 import router from '@adonisjs/core/services/router'
-import fs from 'node:fs/promises'
-import { MarkdownFile } from '@dimerapp/markdown'
 import { toHtml } from '@dimerapp/markdown/utils'
+import { MovieService } from '#services/movie_service'
 
 router
   .get('/', async (ctx) => {
-    const url = app.makeURL(`resources/movies`)
-
-    const files = await fs.readdir(url)
+    const slugs = await MovieService.getSlug()
 
     const movies: Record<string, any>[] = []
 
-    for (const filename of files) {
-      const movieUrl = app.makeURL(`resources/movies/${filename}`)
-      const file = await fs.readFile(movieUrl, 'utf-8')
-      const md = new MarkdownFile(file)
+    for (const slug of slugs) {
+      const md = await MovieService.read(slug)
 
       await md.process()
       movies.push({
         title: md.frontmatter.title,
         summary: md.frontmatter.summary,
-        slug: filename.replace('.md', ''),
+        slug,
       })
     }
 
@@ -42,23 +36,12 @@ router
 
 router
   .get('/movie/:slug', async (ctx) => {
-    const url = app.makeURL(`resources/movies/${ctx.params.slug}.md`)
-    try {
-      const files = await fs.readFile(url, 'utf-8')
+    const md = await MovieService.read(ctx.params.slug)
 
-      const md = new MarkdownFile(files)
+    const movie = toHtml(md).contents
 
-      await md.process()
+    ctx.view.share({ movie, md })
 
-      const movie = toHtml(md).contents
-
-      ctx.view.share({ movie })
-    } catch (error) {
-      throw new Exception(`could not find movie called ${ctx.params.slug}`, {
-        code: 'E_NOT_FOUND',
-        status: 404,
-      })
-    }
     return ctx.view.render('pages/movies/show')
   })
   .as('movies.show')
